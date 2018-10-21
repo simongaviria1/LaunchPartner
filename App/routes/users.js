303a1a6c2f6f0325589678923ddb4c1c208d5a6b
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-10-21 14:24:08 -0400

    server wasnt starting, rectified issue

--- App/routes/users.js
+++ App/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {loginRequired} = require("../auth/helpers");
+const {loginRequired} = require("../auth/helper");
 const passport = require("../auth/local");
 const db = require("../db/queries");
 
